280c588b45f4d2147fc7202a86fe0359b73fbdac
Maarten van Dessel <mvdssel@Maartens-MacBook-Pro.local>
2017-11-11 20:09:19 +0200

    added comments

--- bin/spoofMAC.bash
+++ bin/spoofMAC.bash
@@ -2,10 +2,12 @@
 
 . printexec
 
+#################
 # parameters
 INTERFACE=$1
 [[ -z "$INTERFACE" ]] && INTERFACE=en0
 
+#################
 # functions
 getMac="ifconfig $INTERFACE | grep ether| cut -d ' ' -f 2"
 generateMac="openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/./0/2; s/.$//'"
@@ -14,10 +16,17 @@ wifiOff="networksetup -setairportpower $INTERFACE off"
 wifiReset="($wifiOff; $wifiOn)"
 detectChanges="networksetup -detectnewhardware"
 
+#################
 #script
 echo "old MAC address:" $( eval "$getMac" )
+
+# detach so it can set a new MAC address
 printexec sudo /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -z
+# set a new MAC address
 printexec sudo ifconfig $INTERFACE ether $( eval "$generateMac")
+# let the hardware detect these changes
 printexec $detectChanges
+# reset Wifi, so it reconnects to the network automatically
 printexec eval "$wifiReset&"
+
 echo "new MAC address:" $( eval "$getMac" )
